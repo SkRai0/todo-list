843ec265592d921f2081fe9c2b9a3a1c8b7f1a88
Sudhanshu Rai <sudhanshurai025@gmail.com>
2022-01-02 09:39:22 +0530

    Implemented the add function

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 //selectors
-
+const todoInput = document.querySelector('.todo-task')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
 
 //event listeners
-
+todoButton.addEventListener('click', todoAdd);
 
 //functions
+
+function todoAdd(event){
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todoInput.value
+    newTodo.classList.add('todo-item')
+    todoDiv.appendChild(newTodo)
+
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton)
+
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('trash-btn')
+    todoDiv.appendChild(trashButton)
+
+    todoList.appendChild(todoDiv)
+    todoInput.value = ''
+}
